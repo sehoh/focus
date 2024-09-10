bae11a2b66fff3ff5ab5af5477080f21789bec8d
sehoh <abdortus159@naver.com>
2024-09-10 14:37:23 +0900

    test : "일별 누적시간"

---
+++ src/test/java/com/example/focus/focussession/service/FocusSessionServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.example.focus.focussession.service;
+
+import com.example.focus.focussession.dto.CumulativeTimeDto;
+import com.example.focus.focussession.repository.FocusSessionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+class FocusSessionServiceImplTest {
+//    @Autowired
+    private FocusSessionService focusSessionService;
+    @Autowired
+    private FocusSessionRepository focusSessionRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+//        focusSessionService.findFocusSessionByMemberId()
+
+    }
+
+    @Test
+    void cumulativeTimeByDateTest() {
+//        FocusSessionServiceImpl focusSessionService = new FocusSessionServiceImpl(focusSessionRepository);
+
+        List<CumulativeTimeDto> rs = focusSessionRepository.findCumulativeTimeByDate();
+
+        //
+        assertThat(rs).isNotEmpty();
+        System.out.println("rs = " + rs);
+        assertThat(rs.get(0).getDate()).isNotNull();
+        assertThat(rs.get(0).getCumulativeTimeDiff()).isGreaterThan(0L);
+
+    }
+}
